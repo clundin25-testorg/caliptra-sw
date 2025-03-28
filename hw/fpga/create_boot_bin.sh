3c0abc58f455a9aea4962008b0b91af2441f2ecf
Carl Lundin <clundin@google.com>
2025-03-28 10:57:58 -0700

    Tweak boot bin script.

--- hw/fpga/create_boot_bin.sh
+++ hw/fpga/create_boot_bin.sh
@@ -4,12 +4,12 @@
 # This script generates a Versal BOOT.BIN using Petalinux.
 # When using an ubuntu image BOOT.BIN replaces boot1901.bin in the boot partition.
 
-if [[ -z $1 ]]; then
-    echo "create_boot_bin.sh [/path/to/caliptra_fpga_project_bd_wrapper.xsa]"
-    exit
-fi
+# if [[ -z $1 ]]; then
+#     echo "create_boot_bin.sh [/path/to/caliptra_fpga_project_bd_wrapper.xsa]"
+#     exit 1
+# fi
 
-xsa_location=$(realpath $1)
+#xsa_location=$(realpath $1)
 
 set -e
 trap '{
