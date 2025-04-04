c90d214466a7f8e5cb68f5d771fa503bda234101
Carl Lundin <clundin@google.com>
2025-04-04 11:35:07 -0700

    Tweak build script and tooling.

--- hw/fpga/create_boot_bin.sh
+++ hw/fpga/create_boot_bin.sh
@@ -4,14 +4,12 @@
 # This script generates a Versal BOOT.BIN using Petalinux.
 # When using an ubuntu image BOOT.BIN replaces boot1901.bin in the boot partition.
 
-if [[ -z $1 ]] && [[ -z ${CALIPTRA_CI}]]; then
+# CALIPTRA_CI re-uses the checked in petalinux project.
+if [[ -z $1 ]] && [[ -z ${CALIPTRA_CI} ]]; then
     echo "create_boot_bin.sh [/path/to/caliptra_fpga_project_bd_wrapper.xsa]"
     exit
 fi
 
-#xsa_location=$(realpath $1)
-
-set -ex
 trap '{
   if [ $? -ne 0 ]
   then
@@ -22,22 +20,38 @@ trap '{
   fi  
 }' EXIT
 
-source /vck190-tools/petalinux-tool/settings.sh
+# Always create a fresh petalinux_project when run outside of CI.
+if [[ -z ${CALIPTRA_CI} ]]; then
+    xsa_location=$(realpath $1)
 
-# petalinux-create -t project --template versal --name petalinux_project
-cd petalinux_project
-echo Adding xsa
-#TODO(clundin): Copy new XSA in
-# petalinux-config --get-hw-description $xsa_location --silentconfig
+    set -ex
+    (rm -rf petalinux_project || true)
 
-echo Modifying Petalinux configuration
+    petalinux-create -t project --template versal --name petalinux_project
+    pushd petalinux_project
+    echo Adding xsa
+    petalinux-config --get-hw-description $xsa_location --silentconfig
 
-# Set ROOTFS to EXT4
-sed -i 's|CONFIG_SUBSYSTEM_ROOTFS_INITRD=y|# CONFIG_SUBSYSTEM_ROOTFS_INITRD is not set|g' project-spec/configs/config
-sed -i 's|# CONFIG_SUBSYSTEM_ROOTFS_EXT4 is not set|CONFIG_SUBSYSTEM_ROOTFS_EXT4=y|g' project-spec/configs/config
-sed -i 's|CONFIG_SUBSYSTEM_INITRD_RAMDISK_LOADADDR=0x0|CONFIG_SUBSYSTEM_SDROOT_DEV="/dev/mmcblk0p2"|g' project-spec/configs/config
-sed -i 's|CONFIG_SUBSYSTEM_INITRAMFS_IMAGE_NAME="petalinux-image-minimal"||g' project-spec/configs/config
-sed -i 's|root=/dev/ram0 rw|root=/dev/mmcblk0p2 rw rootwait|g' project-spec/configs/config
+    echo Modifying Petalinux configuration
+
+    # Set ROOTFS to EXT4
+    sed -i 's|CONFIG_SUBSYSTEM_ROOTFS_INITRD=y|# CONFIG_SUBSYSTEM_ROOTFS_INITRD is not set|g' project-spec/configs/config
+    sed -i 's|# CONFIG_SUBSYSTEM_ROOTFS_EXT4 is not set|CONFIG_SUBSYSTEM_ROOTFS_EXT4=y|g' project-spec/configs/config
+    sed -i 's|CONFIG_SUBSYSTEM_INITRD_RAMDISK_LOADADDR=0x0|CONFIG_SUBSYSTEM_SDROOT_DEV="/dev/mmcblk0p2"|g' project-spec/configs/config
+    sed -i 's|CONFIG_SUBSYSTEM_INITRAMFS_IMAGE_NAME="petalinux-image-minimal"||g' project-spec/configs/config
+    sed -i 's|root=/dev/ram0 rw|root=/dev/mmcblk0p2 rw rootwait|g' project-spec/configs/config
+
+    popd
+else
+    # Copy the latest VCK 190 bitstream from GCP bucket.
+    gcloud storage cp gs://${CALIPTRA_CI_BUCKET}/vck190_bitstream.tar.gz .
+    tar -C petalinux_project/project-spec/hw-description -xvf vck190_bitstream.tar.gz
+
+    # Bring in petalinux toolchain.
+    source /vck190-tools/petalinux-tool/settings.sh
+fi
+
+pushd petalinux_project
 
 echo Building FW components, only device-tree depends on XSA
 petalinux-build -c device-tree
@@ -53,4 +67,4 @@ dtc -I dts -O dtb -o images/linux/system.dtb images/linux/system.dts
 
 echo Packaging boot files
 petalinux-package --boot --format BIN --plm --psmfw --u-boot --dtb --force
-cd ../
+popd
